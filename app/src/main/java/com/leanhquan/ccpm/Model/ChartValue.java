93f893b710d2fab9904c5a4774b703adc3722248
leanhquan3011 <leanhquan301198@gmail.com>
2021-04-22 18:41:12 +0700

    finish note function

---
+++ app/src/main/java/com/leanhquan/ccpm/Model/ChartValue.java
@@ -0,0 +1,15 @@
+package com.leanhquan.ccpm.Model;
+
+public class ChartValue {
+    public String name;
+    public int value = 0;
+
+    public ChartValue() {
+    }
+
+    public ChartValue(String name, int value) {
+        this.name = name;
+        this.value = value;
+    }
+
+}
