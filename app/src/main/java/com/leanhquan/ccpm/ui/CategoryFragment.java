f94629c407cf0f000837eb2a16f3ea31e4f3d612
leanhquan3011 <leanhquan301198@gmail.com>
2021-04-22 18:19:36 +0700

    UI home

---
+++ app/src/main/java/com/leanhquan/ccpm/ui/CategoryFragment.java
@@ -0,0 +1,23 @@
+package com.leanhquan.ccpm.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.leanhquan.ccpm.R;
+
+
+public class CategoryFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_category, container, false);
+    }
+}
